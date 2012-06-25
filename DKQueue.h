639bcad9d6ff2c4f555d24e737d6486260bb899f
Dominik Krejcik <dominik.krejcik@tap-do.com>
2012-06-25 18:03:12 +0100

    Method description
    Added comments describing the methods

--- DKQueue.h
+++ DKQueue.h
@@ -3,7 +3,8 @@
 //
 //  Created by Dominik Krejčík on 25/09/2011.
 //  Feel free to use and distribute as long as you mention me. Or buy me a beer.
-//
+
+//DKQueue - FIFO (first in first out) data structure for Objective-C
 
 #import <Foundation/Foundation.h>
 
@@ -11,12 +12,18 @@
     NSMutableArray* array;
 }
 
+// Removes and returns the element at the front of the queue
 -(id)dequeue;
+// Add the element to the back of the queue
 -(void)enqueue:(id)element;
+// Remove all elements
 -(void)clear;
 
+// Returns the element at the front of the queue
 -(id)peek;
+// Returns YES if the queue is empty
 -(BOOL)isEmpty;
+// Returns the size of the queue
 -(NSInteger)size;
 
 @end
